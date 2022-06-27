9da6ee8e72f9d453a03bf2fe52d400acc50b7aef
jeanpierremvt <jeanpierremvt@gmail.com>
2022-06-27 16:00:53 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World. JP vous souhaite la bienvenue!'));
+app.get('/', (req, res) => res.send('Hello World. Bienvenu dans mon application nodeJs'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
